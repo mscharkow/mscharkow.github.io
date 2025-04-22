b0acf73ac30df5fe75e7a846f9d54bf541a080b4
Michael Scharkow <michael@underused.org>
2025-04-22 13:29:16 +0200

    A1c

--- funcs.r
+++ funcs.r
@@ -1,13 +1,21 @@
 library(tidyverse)
 library(xaringanExtra)
 
-make_schedule = function(start_date, end_date, no_classes, topics){
+make_schedule = function(start_date, end_date, no_classes, topics, onlyslides = F){
   classes = seq.Date(from = start_date, to = end_date, by = "1 week") %>%
     discard(~.x %in% no_classes)
 
-  tibble(Datum = classes) |>
+  plan = tibble(Datum = classes) |>
     head(length(topics)) |>
     mutate(Thema = topics) |>
     rownames_to_column("Sitzung") |>
     mutate(Thema = paste0("<a href='#", Sitzung, "'>", topics, "<a/>"))
+
+  if(onlyslides==T) {
+    plan = plan |>
+      mutate(Thema = paste0("<a href='slides.html#", Sitzung, "'>", topics, "<a/>"))
+  }
+
+  write_tsv(plan, "plan.tsv")
+  plan
 }
